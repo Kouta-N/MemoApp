dd170b78d0fdfdd9aea8a951ba1bea48a9b5f370
Kouta Nikkuni <nk29time@gmail.com>
2023-09-18 22:58:36 +0900

    add icon

---
+++ App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import { StyleSheet, Text, View } from "react-native";
+
+import MemoListScreen from "./src/screens/MemoListScreen";
+import MemoDetailScreen from "./src/screens/MemoDetailScreen";
+
+import AppBar from "./src/components/AppBar";
+
+export default function App() {
+  return <MemoListScreen />;
+  // return <MemoDetailScreen />;
+}
